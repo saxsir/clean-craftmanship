1ba8786b0a95d24097bf3bcd67bc17ef85c421a6
saxsir <1664554+saxsir@users.noreply.github.com>
2022-10-12 17:17:20 +0900

    pushしたらemptyじゃなくなる

--- stack/stack.go
+++ stack/stack.go
@@ -1,11 +1,23 @@
 package stack
 
-type Stack struct{}
-
-func NewStack() *Stack {
-	return &Stack{}
+type Stack struct {
+	empty bool
 }
 
+func NewStack() *Stack {
+	return &Stack{
+		empty: true,
+	}
+}
+
+var s = NewStack()
+
 func (s *Stack) isEmpty() bool {
-	return true
+	return s.empty
+}
+
+func (s *Stack) push(_element int) {
+	s.empty = false
+}
+
 }
--- stack/stack_test.go
+++ stack/stack_test.go
@@ -10,3 +10,14 @@ func Test_canCreateStack(t *testing.T) {
 		t.Errorf("want %v, but got %v", want, got)
 	}
 }
+
+func Test_afterOnePush_isNotEmpty(t *testing.T) {
+	s := NewStack()
+	s.push(0)
+	want := false
+	got := s.isEmpty()
+	if want != got {
+		t.Errorf("want %v, but got %v", want, got)
+	}
+}
+
